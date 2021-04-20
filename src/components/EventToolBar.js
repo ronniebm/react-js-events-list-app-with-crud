ad02a86b76079ce5c66345ba7c5fabc3a2f56668
Ronnie <ronnie.coding@gmail.com>
2021-04-20 16:05:29 -0500

    saving changes checkpoint#1

--- src/components/EventToolBar.js
+++ src/components/EventToolBar.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/EventToolBar.css';
 
-
 export const EventToolBar = ({openCloseInsertData}) => {
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit hecho')
+  }
+
   return (
     <div className="toolbar">
       <div className="toolbar__wrapper">
         <h2 className="toolbar__title">Event Register APP</h2>
         <div className="toolbar__container">
           <button className="toolbar__container__button" onClick={()=>openCloseInsertData()}>+</button>
-          <input className="toolbar__container__search js-search" type="text" name="name" placeholder="Search an event" />
+          <form onSubmit={handleSubmit} >
+            <input 
+              className="toolbar__container__search js-search" 
+              type="text" 
+              name="name" 
+              placeholder="Search an event"
+              value={ inputValue }
+              onChange={ handleInputChange }
+            />
+          </form>
         </div>
       </div>
     </div>
   )
+
 }
