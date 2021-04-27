830c357b86a8bd3839cdae0f7306b324c7329e0a
Ronnie <ronnie.coding@gmail.com>
2021-04-27 11:29:01 -0500

    checkpoint before implementing DatePicker from 'react-datepicker'

--- src/components/InsertDataFormik.js
+++ src/components/InsertDataFormik.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import '../styles/InsertDataFormik.css';
 
 const ErrorDiv = ({error}) => {
@@ -7,6 +9,12 @@ const ErrorDiv = ({error}) => {
     <p className="insert-data-formik__error">{error}</p>
   )
 }
+
+const dropDownOptions = [
+  { key: 'Select an option', value: ''},
+  { key: 'Option 1', value: 'public'},
+  { key: 'Option 2', value: 'private'}
+]
 
 export const InsertDataFormik = () => (
 
@@ -34,8 +42,9 @@ export const InsertDataFormik = () => (
           errors.hostName = 'A host name is required';
         }
         if (!values.eventType) {
-          errors.eventType = 'Expected "public" or "private"';
+          errors.eventType = 'Please choose an option';
         }
+        console.log(values.eventType);
         if (!values.date) {
           errors.date = 'A date is required';
         }
@@ -61,26 +70,43 @@ export const InsertDataFormik = () => (
       }) => (
 
         <Form>
-          <div>
+          <div className="insert-data-formik__field-div">
             <Field type="text" name="event" placeholder="Event name" className="insert-data-formik__field"/>
             {(errors.event && touched.event && errors.event) ? <ErrorDiv error={errors.event}/>: null}
           </div>
 
-          <div><Field type="text" name="location" placeholder="Event location"/></div>
-          {(errors.location && touched.location && errors.location) ? <ErrorDiv error={errors.location}/>: null}
+          <div className="insert-data-formik__field-div">
+            <Field type="text" name="location" placeholder="Event location" className="insert-data-formik__field"/>
+            {(errors.location && touched.location && errors.location) ? <ErrorDiv error={errors.location}/>: null}
+          </div>
 
-          <div><Field type="text" name="hostName" placeholder="Event host name"/></div>
-          {(errors.hostName && touched.hostName && errors.hostName) ? <ErrorDiv error={errors.hostName}/>: null}
+          <div className="insert-data-formik__field-div">
+            <Field type="text" name="hostName" placeholder="Event host name" className="insert-data-formik__field"/>
+            {(errors.hostName && touched.hostName && errors.hostName) ? <ErrorDiv error={errors.hostName}/>: null}
+          </div>
 
-          <div><Field type="text" name="eventType" placeholder='"public" or "private"'/></div>
-          {(errors.eventType && touched.eventType && errors.eventType) ? <ErrorDiv error={errors.eventType}/>: null}
+          <div className="insert-data-formik__field-div">
+            <Field as="select" name="eventType" className="insert-data-formik__select" options={dropDownOptions}>
+              <option value=""></option>
+              <option value="public">public</option>
+              <option value="private">private</option>
+            </Field>
+            {(errors.eventType && touched.eventType && errors.eventType) ? <ErrorDiv error={errors.eventType}/>: null}
+          </div>
 
-          <div><Field type="text" name="date" placeholder="Event's date"/></div>
-          {(errors.date && touched.date && errors.date) ? <ErrorDiv error={errors.date}/>: null}
+          <div className="insert-data-formik__field-div">
+            <Field type="text" name="date" placeholder="Event's date" className="insert-data-formik__field"/>
+            {(errors.date && touched.date && errors.date) ? <ErrorDiv error={errors.date}/>: null}
+          </div>
 
-          <div>
-            <button type="submit" disabled={isSubmitting}>
-              Submit
+          <DatePicker name="my-date" />
+
+          <div className="insert-data-formik__field-div">
+            <button type="submit" disabled={isSubmitting} className="insert-data-formik__button">
+              Cancel
+            </button>
+            <button type="cancel" disabled={isSubmitting} className="insert-data-formik__button">
+              Save
             </button>
           </div>
 
